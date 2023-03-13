3d16f68a9b7ed1e770ac5ec617227bf1008e7f0c
生信探索 <aubot@hotmail.com>
2023-03-13 14:24:28 +0800

    anno_heatmap

--- __init__.py
+++ __init__.py
@@ -5,4 +5,4 @@ from ._plot import *
 from ._integration import *
 from .gene_level_analysis import *
 from .utils import subset
-from .cell_type_annotation import labeled,label_helper
+from .cell_type_annotation import labeled,label_helper,anno_heatmap
